a2ef28551e0ac07f3e6da6278b2d5b156ab555d4
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-17 20:39:21 -0300

    Create user Schema and model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+	name: {
+		type: 'string',
+		required: true,
+	},
+	email: {
+		type: 'string',
+		required: true,
+		unique: true
+	},
+	password: {
+		type: 'string',
+		required: true,
+	},
+	img: {
+		type: 'string'
+	},
+	role: {
+		type: 'string',
+		required: true,
+		default: 'USER_ROLE'
+	},
+	google: {
+		type: Boolean,
+		default: false
+	}
+});
+
+module.exports = model('user', userSchema);
