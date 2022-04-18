ff5856650b0690edd8b4d44be550f8ae37f5c52f
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-18 14:04:34 -0300

    Implement bcrypt to encrypt the user password

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { response } = require('express');
+const bcrypt = require('bcrypt');
 
 const getUsers = async (req, res) => {
 	const users = await User.find({}, '_id name email role'); //Este metodo crea una consulta find que obtiene una lista de todos los usuarios. En este caso no pusimos nada como filtro. por eso el objeto vacio, pero como segundo parametro especificamos que propiedades del User model nos interesan.
@@ -10,7 +11,7 @@ const getUsers = async (req, res) => {
 }
 
 const createUser = async (req, res = response) => {
-	const { name, email, password } = req.body; //Desestructuracion del objeto body.
+	const { email, password } = req.body; //Desestructuracion del objeto body.
 	try {
 		const emailExist = await User.findOne({ email: email });
 
@@ -22,6 +23,12 @@ const createUser = async (req, res = response) => {
 		}
 		
 		const user = new User(req.body);
+
+		//Encrypt password
+		const salt = bcrypt.genSaltSync();
+		user.password = bcrypt.hashSync(password, salt);
+		
+		//Save user
 		await user.save();
 	
 		res.json({
