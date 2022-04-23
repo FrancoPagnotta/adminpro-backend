8ad6f7addf27b563a7edde354aa75e4dab17cd6d
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-23 17:04:42 -0300

    Add validateJWT and fieldsValidations middlewares in post method

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,19 +1,25 @@
 // Route: /api/users
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { validateJWT } = require('../middlewares/validate-jwt');
 const { 
 	getHospitals, 
 	createHospital, 
 	updateHospital, 
 	deleteHospital
 	} = require('../controllers/hospitals');
-// const { check } = require('express-validator');
-// const { validateJWT } = require('../middlewares/validate-jwt');
+const { fieldsValidations } = require('../middlewares/fields-validations');
 
 const router = Router();
 
 router.get('/', getHospitals);
 
-router.post('/', createHospital);
+router.post('/', validateJWT,
+	[
+		check('name', 'The name of the hospital is required').not().isEmpty()
+	],
+	fieldsValidations,
+	createHospital);
 
 router.put('/:id', updateHospital);
 
