746096bca78a4896f8eb600df425495ba699ae89
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-23 17:05:01 -0300

    Implement logic to create a hospital and save it in db

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,4 +1,5 @@
 const { response } = require('express');
+const Hospital = require('../models/hospital');
 
 const getHospitals = (req, res = response) => {
 	res.status(200).json({
@@ -7,11 +8,23 @@ const getHospitals = (req, res = response) => {
 	});
 }
 
-const createHospital = (req, res = response) => {
-	res.status(200).json({
-		ok: true,
-		message: 'Endpoint createHospital works'
-	})
+const createHospital = async (req, res = response) => {
+	const uid = req.uid;
+	const hospital = new Hospital({ user: uid, ...req.body });
+
+	try {
+		await hospital.save();
+
+		res.status(200).json({
+			ok: true,
+			hospital
+		});
+	} catch (error) {
+		res.status(500).json({
+			ok: true,
+			message: 'Unespected error'
+		});
+	}
 }
 
 const updateHospital = (req, res = response) => {
