b490660b60adf1835e7dff91bd6265465d908716
Franco Pagnotta <francoapagnotta@gmail.com>
2022-04-28 13:04:10 -0300

    Create getCollectionData function

--- controllers/searches.js
+++ controllers/searches.js
@@ -22,7 +22,44 @@ const getTodo = async (req = request, res = response) => {
 	});
 }
 
+const getCollectionData = async (req = request, res = response) => {
+	const table = req.params.table;
+	const search = req.params.search;
+	const regex = new RegExp(search, 'i');
+
+	let data = [];
+
+	switch (table) {
+		case 'users':
+			data = await User.find({ name: regex });
+			break;
+
+		case 'doctors':
+			data = await Doctor.find({ name: regex })
+								.populate('hospital', 'name')
+								.populate('user', 'name')
+			break;
+
+		case 'hospitals':
+			data = await Hospital.find({ name: regex })
+									.populate('user', 'name')
+			break;
+
+		default:
+			return res.status(400).json({
+				ok: false,
+				message: 'Invalid table, must be users, doctors or hospitals'
+			}); // the break is unnecessary here, dont have sense, because the return cut the flow of the function.
+	}
+
+	res.status(200).json({
+		ok: true,
+		result: data
+	});
+}
+
 
 module.exports = {
-	getTodo
+	getTodo,
+	getCollectionData
 }
