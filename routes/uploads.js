a6d206c2c3f426374a1fd91704dd88a94d978dce
Franco Pagnotta <francoapagnotta@gmail.com>
2022-05-04 21:02:02 -0300

    Create getFile endpoint

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,7 @@
 // endpoint --> /api/upload/
 
 const { Router } = require('express');
-const { uploadFiles } = require('../controllers/uploads');
+const { uploadFiles, getFile } = require('../controllers/uploads');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const fileUpload = require('express-fileupload');
 
@@ -10,6 +10,7 @@ const router = Router();
 router.use(fileUpload());
 
 router.put('/:collection/:uid', validateJWT, uploadFiles);
+router.get('/:collection/:image', validateJWT, getFile);
 
 
 
